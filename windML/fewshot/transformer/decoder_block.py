8ba3666b234a0cce08f8d3d1599bf9119b9e0687
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-04-02 13:24:21 +0100

    Add files via upload

--- windML/fewshot/transformer/decoder_block.py
+++ windML/fewshot/transformer/decoder_block.py
@@ -1,58 +1,77 @@
-class DecoderBlock():
-    def __init__(self,num_heads,d_model,drop_prob):  
-        
-        self.Ln1 = LayerNorm()
-        self.Ln2 = LayerNorm()
-        self.Ln3 = LayerNorm()
-        
-        self.dp1 = Dropout(drop_prob)
-        self.dp2 = Dropout(drop_prob)
-        self.dp3 = Dropout(drop_prob)
-        
-        self.ff1 = LinearLayer3d(1,d_model,d_model*4)
-        self.ff2 = LinearLayer3d(1,d_model*4,d_model)
-        
-        self.masked_mh_attn = MultiHeadAttention(num_heads,d_model)
-        self.mh_attn = MultiHeadAttention(num_heads,d_model)
-        
-        
-    def forward(self,inp,enc_o,src_valid_lens=None,trg_valid_lens=None):
-        
-        attn1_out = self.masked_mh_attn.forward(inp,inp,inp,trg_valid_lens,attn_mask=True)
-        dp1_o = self.dp1.forward(attn1_out)
-        norm1_out = self.Ln1.forward(dp1_o+inp)
-        
-        attn2_out = self.mh_attn.forward(norm1_out,enc_o,enc_o,src_valid_lens)
-        dp2_o = self.dp2.forward(attn2_out)
-        norm2_out = self.Ln2.forward(dp2_o+norm1_out)
-        
-        ff1_out = self.ff1.forward(norm2_out)
-        self.reLu = ff1_out < 0
-        ff1_out[self.reLu] = 0
-        ff2_out = self.ff2.forward(ff1_out)
-        dp3_o = self.dp3.forward(ff2_out)
-        norm3_out = self.Ln3.forward(dp3_o+norm2_out)
-        
-        return norm3_out
-        
-        
-    def backward(self,grads,step_size):
-        
-        d_add3 = self.Ln3.backward(grads)
-        d_ff2 = self.dp3.backward(d_add3)
-        d_ff1_out = self.ff2.backward(d_ff2,step_size)
-        d_ff1_out[self.reLu] = 0
-        d_norm2 = self.ff1.backward(d_ff1_out,step_size) + d_add3
-        d_add2 = self.Ln2.backward(d_norm2)
-        
-        d_mh_attn = self.dp2.backward(d_add2) 
-        d_norm1, d_enc_o1, d_enc_o2 = self.mh_attn.backward(d_mh_attn, step_size)  
-        d_norm1 += d_add2
-        d_add1 = self.Ln1.backward(d_norm1)
-        
-        d_masked_mh_attn = self.dp1.backward(d_add1)
-        d_inp, d_k, d_v = self.masked_mh_attn.backward(d_masked_mh_attn, step_size)
-        
-        d_inp = d_inp + d_k + d_v + d_add1
-        d_enc_o = d_enc_o1 + d_enc_o2
-        return d_inp, d_enc_o
+from typing import List, Dict
+
+from numpy import ndarray, add
+
+from windML.fewshot.transformer.utils import ModelSettings
+from windML.fewshot.transformer.layers.dense import DenseLayer
+from windML.fewshot.transformer.layers.position_encoder import PositionEncodingLayer
+from windML.fewshot.transformer.layers.multihead_attention import MultiHeadAttention
+from windML.fewshot.transformer.layers.residual import ResidualLayer
+from windML.fewshot.transformer.layers.dropout import DropoutLayer
+from windML.fewshot.transformer.layers.normalisation import NormalisationLayer
+
+
+class DecoderBlock:
+    def __init__(self) -> None:
+        self.positional_encoding_layer = PositionEncodingLayer() 
+        self.multihead_attention1 = MultiHeadAttention()
+        self.multihead_attention2 = MultiHeadAttention()
+        self.residual_layer1 = ResidualLayer()
+        self.residual_layer2 = ResidualLayer()
+        self.residual_layer3 = ResidualLayer()
+        self.dropout_layer1 = DropoutLayer()
+        self.dropout_layer2 = DropoutLayer()
+        self.dropout_layer3 = DropoutLayer()
+        self.normalisation_layer1 = NormalisationLayer()
+        self.normalisation_layer2 = NormalisationLayer()
+        self.normalisation_layer3 = NormalisationLayer()
+
+    def forward(self, context: ndarray, inputs: ndarray) -> ndarray:
+        context = self.positional_encoding_layer.forward(context)        
+
+        attention1 = self.multihead_attention1.forward(
+            Q=self.deep_layers(context), 
+            K=self.deep_layers(context), 
+            V=self.deep_layers(context)
+        )
+        summed = add(context, attention1)
+        residual1 = self.residual_layer1.forward(summed, output_dimension=summed.shape[-1])
+        dropout1 = self.dropout_layer1.forward(residual1)
+        normalised1 = self.normalisation_layer1.forward(dropout1)
+
+        attention2 = self.multihead_attention2.forward(
+            Q=self.deep_layers(context),
+            K=self.deep_layers(inputs),
+            V=self.deep_layers(inputs)
+        )
+        summed2 = add(normalised1, attention2)
+        residual2 = self.residual_layer2.forward(summed2, output_dimension=summed2.shape[-1])
+        dropout2 = self.dropout_layer2.forward(residual2)
+        normalised2 = self.normalisation_layer2.forward(dropout2)
+
+        residual3 = self.residual_layer3.forward(normalised2, output_dimension=normalised2.shape[-1])
+        dropout3 = self.dropout_layer3.forward(residual3)
+        normalised3 = self.normalisation_layer3.forward(dropout3)
+        return normalised3
+
+    @staticmethod
+    def deep_layers(inputs:ndarray) -> List[callable]:
+        return dict(map(
+            lambda index:(index, DenseLayer().forward(inputs)),
+            range(ModelSettings.N_HEADS.value)
+        ))
+
+
+    # def backward(self,inputs:ndarray,error:ndarray) -> Tuple[ndarray,ndarray]:
+    #     dLayer3 = self.normalisation_layer3.backward(error)
+    #     dLogits = self.dense_layer2.backward(inputs,dLayer3)
+    #     dLogits[self.activations] = 0
+    #     dNormLayer2 = self.dense_layer1.backward(inputs,dLogits) + dLayer3
+    #     dLayer2 = self.normalisation_layer2.backward(dNormLayer2)        
+    #     dNormLayer1, dNormLayer1Key, dNormLayer1Value = self.attention_layer2.backward(inputs,dLayer2)  
+    #     dNormLayer1 += dLayer2
+    #     dLayer1 = self.normalisation_layer1.backward(dNormLayer1)        
+    #     dInput, dInputKey, dInputValue = self.attention_layer1.backward(inputs,dLayer1)        
+    #     dInput = dInput + dInputKey + dInputValue + dLayer1
+    #     dContext = dNormLayer1Key + dNormLayer1Value
+    #     return dInput, dContext
