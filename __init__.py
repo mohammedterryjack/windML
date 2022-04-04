dc9b1f073d4d93a57532ea8f768fe3b964eca068
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-04-04 17:20:31 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from windML.oneshot.models.extreme_learning_machine import ELMClassifier
-from windML.oneshot.modelfree.matrix_factorisation import MFRClassifier
-from windML.fewshot.direct_feedback_alignment.dfa_classifier import DFAClassifier
-
-__version__ = "0.2.4"
